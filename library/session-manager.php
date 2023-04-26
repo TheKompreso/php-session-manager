c4b87653f851eaf6da0c129b187ea37e6a6239e1
Misha <kompreso@vk.com>
2023-04-26 23:35:44 +0300

    First commit

---
+++ library/session-manager.php
@@ -0,0 +1,46 @@
+<?php
+    class SessionManager
+    {
+        static $mysqli;
+        public static function InsertDataBase($database)
+        {
+            self::$mysqli = $database;
+        }
+        public static function CreateSession($user, $timelife, $rights, $solt)
+        {
+            $result['key'] = hash('sha256', $user."|".$timelife."|".$rights."|".$solt."|".time());
+            $sql = "INSERT INTO sessions (`id`,`hash`,`user`,`rights`,`starttime`,`endtime`) VALUES (NULL,'".$result['key']."','".$user."',$rights,".time().",".(time()+$timelife).")";
+            if (self::$mysqli->query($sql) !== false) 
+            {
+                $result['id'] = self::$mysqli->insert_id;
+                return $result;
+            } 
+            else 
+            {
+                echo "error: " . self::$mysqli->error;
+                exit;
+            }
+        }
+        public static function CheckSession($id, $key)
+        {
+            $sql = "SELECT * FROM sessions WHERE id='".(int)$id."' LIMIT 1";
+            $result = self::$mysqli->query($sql);
+            if($result !== false) // Ошибка запроса
+            {
+                if($data = $result->fetch_assoc()) // Сессия найдена
+                {
+                    if($data['endtime'] < time()) return null;
+                    if(strcmp($data['hash'],$key) !== 0) return null;
+                    unset($data['hash']);
+                    return $data;
+                }
+            }
+            return null;
+        }
+        public static function EndSession($id)
+        {
+            $sql = "UPDATE sessions SET endtime=".time()." WHERE id=$id";
+            self::$mysqli->query($sql);
+        }
+    }
+?>
---
+++ sql/sessions.sql
@@ -0,0 +1,62 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Хост: localhost
+-- Время создания: Апр 26 2023 г., 23:34
+-- Версия сервера: 5.7.27-30
+-- Версия PHP: 7.1.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `u0909374_gorkraitrans.ru`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` bigint(20) NOT NULL,
+  `hash` char(64) NOT NULL,
+  `user` int(11) NOT NULL,
+  `rights` int(11) NOT NULL,
+  `starttime` bigint(20) NOT NULL,
+  `endtime` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
